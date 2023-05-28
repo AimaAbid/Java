695d3660b6006e6b0aa01716ccd02c5fd9348501
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-05-28 12:31:15 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Jake,Itachi,Neji,Hidan,Eren,Gojo!");
+        System.out.println("Test!");
     }
 }
