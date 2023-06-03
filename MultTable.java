b9e8acf978ae422158897ed7a42706a59de7ba6a
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-06-03 11:10:21 +0530

    Add files via upload

---
+++ MultTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultTable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number whose table you want to create");
+        Scanner Sc = new Scanner(System.in);
+        int num = Sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+
+    }
+}
---
+++ T.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class T {
+    public static void main(String args[]) {
+        double tax = 0;
+        System.out.println("Enter the salary");
+        Scanner Sc = new Scanner(System.in);
+        double salary = Sc.nextDouble();
+
+        if (salary <= 250000)
+            tax = 0;
+
+        else if (salary > 250000 && salary <= 500000) {
+            tax = tax + 0.05 * (salary - 250000);
+        }
+
+        else if (salary > 500000 && salary <= 1000000) {
+            tax = tax + 0.05 * (salary - 250000);
+            tax = tax + 0.2 * (salary - 500000);
+        } else {
+            tax = tax + 0.05 * (salary - 250000);
+            tax = tax + 0.2 * (salary - 500000);
+            tax = tax + 0.3 * (salary - 1000000);
+        }
+
+        System.out.println("Tax = " + tax);
+
+    }
+
+}
