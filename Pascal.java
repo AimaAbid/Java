f57f08587cedd3d810038d6e21dca2ce99e8fa51
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-06-04 11:47:10 +0530

    Add files via upload

---
+++ Pascal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pascal {
+    public static void main(String[] args) {
+        System.out.println("Enter the no of lines");
+
+        Scanner Sc = new Scanner(System.in);
+        int nol = Sc.nextInt();
+
+        int spaces = nol;
+        int number = 1;
+
+        for (int i = 0; i < nol; i++) {
+
+            for (int s = 1; s <= spaces; s++) {
+                System.out.print(" ");
+
+            }
+            number = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            spaces--;
+            System.out.println();
+
+        }
+
+    }
+
+}
