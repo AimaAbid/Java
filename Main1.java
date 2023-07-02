a073a83e7e96e18bed4f35936cfdd518bbf4a5d3
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-07-02 20:49:10 +0530

    Add files via upload
    uses package

---
+++ Main1.java
@@ -0,0 +1,8 @@
+import myPackage.MyClass;
+
+public class Main1 {
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass();
+        myObject.displayMessage();
+    }
+}
