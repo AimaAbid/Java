a3f4b45edc9fa94b391c03b638f23fb6b76a3fa5
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-08-16 20:36:22 +0530

    Add files via upload

--- ArrayListPro.java
+++ ArrayListPro.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 public class ArrayListPro {
     public static void main(String[] args) {
-        ArrayList<Integer> a1 = new ArrayList<Integer>();
-        ArrayList<Integer> a2 = new ArrayList<Integer>();
-        ArrayList<Integer> a3 = new ArrayList<Integer>();
+        ArrayList<String> a1 = new ArrayList<>();
+        ArrayList<Integer> a2 = new ArrayList<>();
+        ArrayList<String> a3 = new ArrayList<>();
 
-        a1.add(0, 70);
+        a1.add("Jake");
+        a1.add("Mika");
+        a1.add("Eren");
+        a1.add("Neji");
+        System.out.println("Names :");
+        System.out.println(a1);
 
-        a2.add(0, 50);
-        a2.add(1, 60);
-        a2.add(2, 70);
-        a2.add(3, 80);
+        a2.add(26);
+        a2.add(10);
+        a2.add(20);
+        a2.add(1);
+        System.out.println("Numbers : ");
+        System.out.println(a2);
 
-        System.out.println("a2's elements:");
-        for (int i = 0; i < a2.size(); i++) {
-            System.out.println(a2.get(i));
-        }
-        a1.addAll(0, a2);
-        System.out.println("a1's elements after copying a2 elements into a1:");
-        for (int i = 0; i < a2.size(); i++) {
-            System.out.println(a1.get(i));
-        }
-
-        a1.clear();
-        // incomplete
+        a3 = (ArrayList<String>) a1.clone();
+        System.out.println("Cloning names into another arraylist :");
+        System.out.println(a3);
 
     }
 }
