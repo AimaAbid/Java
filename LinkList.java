b3ff582a2456b1a86911f61eef88c8c27bbe6b46
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2023-08-16 16:15:06 +0530

    Add files via upload

---
+++ LinkList.java
@@ -0,0 +1,25 @@
+import  java.util.*;
+public class LinkList {
+  
+
+
+
+    public static void main (String[] args)  {
+        
+        LinkedList<String> info = new LinkedList<>();
+        info.add("first name");
+         info.add("last name");
+          info.add("age"); 
+         info.add("gender");
+         System.out.println(info);
+        
+    }
+}
+
+    
+
+    
+
+
+    
+
